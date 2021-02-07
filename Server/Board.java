a93c3071a7340ae7148ec213f8bc762aec552361
JACOTOT Hugo <hjacotot@free.fr>
2021-02-07 13:05:48 +0100

    Added Board's basic generation function and constructor + Treasure's functions

--- Server/Board.java
+++ Server/Board.java
@@ -5,6 +5,15 @@ import Server.Elements.Element;
 public class Board {
     private Element[][] elements;
 
+    public Board() {
+    	fillEmpty();
+    }
+
+    public void fillEmpty() {
+    	for(int i=0;i<elements.length;i++)
+    		for(int j=0; j<elements[i].length;j++)
+    			elements[i][j] = new Element();
+    }
 
     public String toString() {
     	for(int i=0;i<elements.length;i++)
--- Server/Elements/Treasure.java
+++ Server/Elements/Treasure.java
@@ -1,6 +1,16 @@
 package Server.Elements;
 
 public class Treasure extends Element {
+
+	private final int value;
+
+	public Treasure(int value) {
+		this.value = value;
+	}
+
+	public int getTreasureValue() {
+		return this.value;
+	}
  
 	public String toString() {
 		return "Treasure";
