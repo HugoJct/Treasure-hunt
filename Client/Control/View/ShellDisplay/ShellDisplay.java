ba9047b320a2cc725e09571ad88d5013596dd7bc
Rémi Lévy <remi.levy91@gmail.com>
2021-02-07 17:30:06 +0100

    Creation of functions to display the content and informations of a board and addition of getters in Board to have an acces from ShellDisplay.

--- Client/Control/View/ShellDisplay/ShellDisplay.java
+++ Client/Control/View/ShellDisplay/ShellDisplay.java
@@ -1,5 +1,62 @@
 package Client.Control.View.ShellDisplay;
 
+import Server.Board;
+import Server.Elements.*;
+
 public class ShellDisplay {
+    Board b;
+    
+    //Constructor
+    public ShellDisplay(Board b) {
+    	this.b = b;
+    }
+    
+    //Display the content of a Board in the shell
+    public void displayBoard() {
+    	this.displayInformations();
+    	for(int y = 0; y<b.getElement().length; y++) {
+    		for(int x = 0; x<b.getElement()[y].length; x++) {
+    			if(b.getElementAt(x, y) == null) {
+    				System.out.print(".");
+    			}
+    			if(b.getElementAt(x, y) instanceof Wall) {
+    				System.out.print("W");
+    			}
+    			if(b.getElementAt(x, y) instanceof Treasure) {
+    				System.out.print("T");
+    			}
+    			if(b.getElementAt(x, y) instanceof Hole) {
+    				System.out.print("H");
+    			}
+    		}
+    		System.out.println("");
+    	}
+    	System.out.println("");
+    }
+    
+    //Display informations about present elements on the board
+    public void displayInformations() {
+    	int voidCounter = 0;
+    	int wallCounter = 0;
+    	int treasureCounter = 0;
+    	int holeCounter = 0;
+    	for(int y = 0; y<b.getElement().length; y++) {
+    		for(int x = 0; x<b.getElement()[y].length; x++) {
+    			if(b.getElementAt(x, y) == null) {
+    				voidCounter++;
+    			}
+    			if(b.getElementAt(x, y) instanceof Wall) {
+    				wallCounter++;
+    			}
+    			if(b.getElementAt(x, y) instanceof Treasure) {
+    				treasureCounter++;
+    			}
+    			if(b.getElementAt(x, y) instanceof Hole) {
+    				holeCounter++;
+    			}
+    		}
+    	}
+    	System.out.println("Voids : "+voidCounter+" / Walls : "+wallCounter+" / Treasures : "+treasureCounter+" / Holes : "+holeCounter);
+    }
     
 }
--- Server/Board.java
+++ Server/Board.java
@@ -4,12 +4,16 @@ import Server.Elements.Element;
 
 public class Board {
     private Element[][] elements;
-    private final int sizeX;
-    private final int sizeY;
 
-    public Board(int x, int y) {
-    	this.sizeX = x;
-    	this.sizeY = y;
+    //Constructor
+    public Board() {
+    	fillEmpty();
+    }
+
+    private void fillEmpty() {
+    	for(int i=0;i<elements.length;i++)
+    		for(int j=0; j<elements[i].length;j++)
+    			elements[i][j] = null; //new Element();
     }
 
     protected void setElementAt(Element elem, int x, int y) {
@@ -19,11 +23,17 @@ public class Board {
     public Element getElementAt(int x, int y) {
     	return elements[x][y];
     }
+    
+    //Getter of the two dimensional array
+    public Element[][] getElement(){
+    	return this.elements;
+    }
 
     public String toString() {
     	for(int i=0;i<elements.length;i++)
     		for(int j=0; j<elements[i].length;j++)
     			return elements[i][j].toString();
+		return null;
     }
 
 }
