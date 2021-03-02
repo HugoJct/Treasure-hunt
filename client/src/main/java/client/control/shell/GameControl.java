f7a328f926c0f288a5142701c74eef4d40b364d6
Matthieu Le Franc <matt.lefranc@gmail.com>
2021-03-02 21:11:38 +0100

    client shell init

--- client/src/main/java/client/control/shell/GameControl.java
+++ client/src/main/java/client/control/shell/GameControl.java
@@ -1,5 +1,16 @@
 package client.control.shell;
 
+import java.util.Scanner;
+
 public class GameControl {
-    
+    private Scanner _input;
+
+    public ControlShell() {
+        this._input = new Scanner(System.in);
+    }
+
+    public void reader() {
+        String com;
+        com = this._input.nextLine();
+    }
 }
