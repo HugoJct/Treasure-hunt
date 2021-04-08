22ffa94fd6f87e7a161dd5bd96f97d9f87e5dd26
Rémi Lévy <remi.levy91@gmail.com>
2021-04-08 21:46:41 +0200

    (WIP) : New corrections of error (version 1) : InvocationTargetException

--- client/src/main/java/client/control/UI/application/Main.java
+++ client/src/main/java/client/control/UI/application/Main.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -17,6 +18,7 @@ import javafx.scene.layout.HBox;
 
 
 public class Main extends Application {
+	FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainFXML.fxml"));
 	Modele m = new Modele(); //Put a game object in argument
 	
 	@Override
@@ -46,57 +48,63 @@ public class Main extends Application {
 		}
 		
 		
-		
-		//Display Walls from Model informations
-		for(int i = 0; i<m.WallPos.length; i++) {
-			File fw = new File("Images/wall2.png");
-			String pathWall = fw.toURI().toURL().toString();
-			Image WallImg = new Image(pathWall, false);
-			ImageView Wall = new ImageView(WallImg);
-			Wall.setLayoutX(m.WallPos[i]*30);
-			Wall.setLayoutY(m.WallPos[i+1]*28);
-			AllElements.getChildren().add(Wall);
-			i++; // i used for x and i+1 for y.
+		//La fonction génère une erreur (cause pour le moment inconnue
+		try {
+			//Display Walls from Model informations
+			for(int i = 0; i<m.WallPos.length; i++) {
+				File fw = new File("Images/wall2.png");
+				String pathWall = fw.toURI().toURL().toString();
+				Image WallImg = new Image(pathWall, false);
+				ImageView Wall = new ImageView(WallImg);
+				Wall.setLayoutX(m.WallPos[i]*30);
+				Wall.setLayoutY(m.WallPos[i+1]*28);
+				AllElements.getChildren().add(Wall);
+				i++; // i used for x and i+1 for y.
+			}
+			
+			//Display treasures
+			for(int i = 0; i<m.TreasurePos.length; i++) {
+				File ft = new File("Images/treasure.png");
+				String pathTreasure = ft.toURI().toURL().toString();
+				Image TreasureImg = new Image(pathTreasure, false);
+				ImageView Treasure = new ImageView(TreasureImg);
+				Treasure.setLayoutX(m.TreasurePos[i]*30);
+				Treasure.setLayoutY(m.TreasurePos[i+1]*28);
+				AllElements.getChildren().add(Treasure);
+				i++; // i used for x and i+1 for y.
+			}
+			
+			//Display Holes
+			for(int i = 0; i<m.HolePos.length; i++) {
+				File fh = new File("Images/hole.png");
+				String pathHole = fh.toURI().toURL().toString();
+				Image HoleImg = new Image(pathHole, false);
+				ImageView Hole = new ImageView(HoleImg);
+				Hole.setLayoutX(m.HolePos[i]*30);
+				Hole.setLayoutY(m.HolePos[i+1]*28);
+				AllElements.getChildren().add(Hole);
+				i++; // i used for x and i+1 for y.
+			}
+			
+			
+			//Display players (Black square for tests)
+			for(int i = 0; i<m.PlayerPos.length; i++) {
+				File fh = new File("Images/Player.png");
+				String pathHole = fh.toURI().toURL().toString();
+				Image HoleImg = new Image(pathHole, false);
+				ImageView Hole = new ImageView(HoleImg);
+				Hole.setLayoutX(m.PlayerPos[i]*30);
+				Hole.setLayoutY(m.PlayerPos[i+1]*28);
+				AllElements.getChildren().add(Hole);
+				i++; // i used for x and i+1 for y.
+			}
+		}catch(Exception e){
+			System.out.println("Erreur lors de l'affichage des éléments surement dû à un manque de données dans GameInfo");
+			e.printStackTrace();
 		}
-		
-		//Display treasures
-		for(int i = 0; i<m.TreasurePos.length; i++) {
-			File ft = new File("Images/treasure.png");
-			String pathTreasure = ft.toURI().toURL().toString();
-			Image TreasureImg = new Image(pathTreasure, false);
-			ImageView Treasure = new ImageView(TreasureImg);
-			Treasure.setLayoutX(m.TreasurePos[i]*30);
-			Treasure.setLayoutY(m.TreasurePos[i+1]*28);
-			AllElements.getChildren().add(Treasure);
-			i++; // i used for x and i+1 for y.
-		}
-		
-		//Display Holes
-		for(int i = 0; i<m.HolePos.length; i++) {
-			File fh = new File("Images/hole.png");
-			String pathHole = fh.toURI().toURL().toString();
-			Image HoleImg = new Image(pathHole, false);
-			ImageView Hole = new ImageView(HoleImg);
-			Hole.setLayoutX(m.HolePos[i]*30);
-			Hole.setLayoutY(m.HolePos[i+1]*28);
-			AllElements.getChildren().add(Hole);
-			i++; // i used for x and i+1 for y.
-		}
-		
-		
-		//Display players (Black square for tests)
-		for(int i = 0; i<m.PlayerPos.length; i++) {
-			File fh = new File("Images/Player.png");
-			String pathHole = fh.toURI().toURL().toString();
-			Image HoleImg = new Image(pathHole, false);
-			ImageView Hole = new ImageView(HoleImg);
-			Hole.setLayoutX(m.PlayerPos[i]*30);
-			Hole.setLayoutY(m.PlayerPos[i+1]*28);
-			AllElements.getChildren().add(Hole);
-			i++; // i used for x and i+1 for y.
-		}
-		
-		
+			
+			
+			
 		/* ----------  Push all graphic elements in the window  --------- */
 		Scene SceneAllElements = new Scene(AllElements);
 		primaryStage.setScene(SceneAllElements);
